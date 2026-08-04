0a43106a0aae9090e59fd96a63e85fa74086a84b
Roger Arran Muñoz Tura <rmunoz001tura@gmail.com>
2026-08-04 17:14:14 +0200

    Update 09_performance_analysis.sql

--- scripts/09_performance_analysis.sql
+++ scripts/09_performance_analysis.sql
@@ -6,11 +6,6 @@ Purpose:
     - To measure the performance of products, customers, or regions over time.
     - For benchmarking and identifying high-performing entities.
     - To track yearly trends and growth.
-
-SQL Functions Used:
-    - LAG(): Accesses data from previous rows.
-    - AVG() OVER(): Computes average values within partitions.
-    - CASE: Defines conditional logic for trend analysis.
 ===============================================================================
 */
 
